4b74d835b23321fcfc616c5fa877a46f021297e5
TakiuGit <paulnoferi@gmail.com>
2015-11-24 13:26:01 +0100

    init

---
+++ src/main/java/model/DotaReader.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by paulu_000 on 24/11/2015.
+ */
+public class DotaReader {
+
+
+}
---
+++ src/main/java/view/MainView.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by paulu_000 on 24/11/2015.
+ */
+public class MainView {
+}
